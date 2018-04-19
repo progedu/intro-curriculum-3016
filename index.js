a89b5ca1698f88793021a5a93298e708451ca357
Iwasaki <iwasaki@arssystem.co.jp>
2018-04-19 05:09:04 +0000

    日付を削除

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const pug = require('pug');
 const server = http.createServer((req, res) => {
-    console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+    console.info('Requested by ' + req.connection.remoteAddress);
     res.writeHead(200, {
         'Content-Type': 'text/html; charset=utf-8'
     });
