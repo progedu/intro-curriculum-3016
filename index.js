02dea1459ab6898c68c51ae062c30c5278e0e115
Ao40 <aoto-miz@softbank.ne.jp>
2019-02-24 08:47:14 +0000

    ログから日付を除去

--- index.js
+++ index.js
@@ -2,57 +2,53 @@
 const http = require('http');
 const pug = require('pug');
 const server = http.createServer((req, res) => {
-    console.info('Requested by ' + req.connection.remoteAddress);
-    res.writeHead(200, {
-        'Content-Type': 'text/html; charset=utf-8'
-    });
+  console.info('Requested by ' + req.connection.remoteAddress);
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8'
+  });
 
-    switch (req.method) {
-        case 'GET':
-            if (req.url === '/enquetes/yaki-shabu') {
-                res.write(pug.renderFile('./form.pug', {
-                    path: req.url,
-                    firstItem: '焼き肉',
-                    secondItem: 'しゃぶしゃぶ'
-                }));
-            } else if (req.url === '/enquetes/rice-bread') {
-                res.write(pug.renderFile('./form.pug', {
-                    path: req.url,
-                    firstItem: 'ごはん🍚',
-                    secondItem: 'パン🥐'
-                }));
-            } else if (req.url === '/enquetes/sushi-pizza') {
-                res.write(pug.renderFile('./form.pug', {
-                    path: req.url,
-                    firstItem: '寿司🍣',
-                    secondItem: 'ピザ🍕'
-                }));
-            }
-            res.end();
-            break;
-        case 'POST':
-            let body = [];
-            req.on('data', (chunk) => {
-                body.push(chunk);
-            }).on('end', () => {
-                body = Buffer.concat(body).toString();
-                const decoded = decodeURIComponent(body);
-                console.info('投稿: ' + decoded);
-                res.write('<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body><h1>' +
-                    decoded + 'が投稿されました</h1></body></html>');
-                res.end();
-            });
-            break;
-        default:
-            break;
-    }
+  switch (req.method) {
+    case 'GET':
+      if (req.url === '/enquetes/yaki-shabu') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: '焼き肉',
+          secondItem: 'しゃぶしゃぶ'
+        }));
+      } else if (req.url === '/enquetes/rice-bread') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: 'ごはん',
+          secondItem: 'パン'
+        }));
+      } else if (req.url === '/enquetes/sushi-pizza') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: '寿司',
+          secondItem: 'ピザ'
+        }));
+      }
+      res.end();
+      break;
+    case 'POST':
+      req.on('data', (data) => {
+        const decoded = decodeURIComponent(data);
+        console.info('投稿: ' + decoded);
+        res.write('<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body><h1>' +
+          decoded + 'が投稿されました</h1></body></html>');
+        res.end();
+      });
+    break;
+  default:
+    break;
+  }
 
 }).on('error', (e) => {
-    console.error('Server Error', e);
+  console.error('Server Error', e);
 }).on('clientError', (e) => {
-    console.error('Client Error', e);
+  console.error('Client Error', e);
 });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-    console.info('Listening on ' + port);
+  console.info('Listening on ' + port);
 });
