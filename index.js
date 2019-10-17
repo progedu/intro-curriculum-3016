dde5b6b860869a1817391c7b5877eb0b0b828b57
Takeharu7-51 <take7spring@gmail.com>
2019-10-17 12:56:30 +0900

    ログから日付を除去

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 const http = require('http');
 const pug = require('pug');
 const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+  console.info('Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
@@ -32,26 +31,24 @@ const server = http.createServer((req, res) => {
       res.end();
       break;
     case 'POST':
-      let rawData = '';
-      req.on('data', (chunk) => {
-        rawData = rawData + chunk;
-      }).on('end', () => {
-        const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
+      req.on('data', (data) => {
+        const decoded = decodeURIComponent(data);
+        console.info('投稿: ' + decoded);
+        res.write('<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
       });
-      break;
-    default:
-      break;
+    break;
+  default:
+    break;
   }
+
 }).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
+  console.error('Server Error', e);
 }).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
+  console.error('Client Error', e);
 });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info('Listening on ' + port);
 });
