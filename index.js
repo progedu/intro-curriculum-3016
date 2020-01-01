b81fe3889e5c8fbbd90aeb7514d4ca9ce0cd88cd
mana2216 <ms00lx.221b@gmail.com>
2020-01-01 12:33:13 +0900

    日付のログを削除

--- index.js
+++ index.js
@@ -2,19 +2,18 @@
 const http = require('http');
 const pug = require('pug');
 const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+  console.info('Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
 
   switch (req.method) {
     case 'GET':
-      if (req.url === '/enquetes/yaki-shabu') {
+      if (req.url === '/enquetes/kara-syouyu') {
         res.write(pug.renderFile('./form.pug', {
           path: req.url,
-          firstItem: '焼き肉',
-          secondItem: 'しゃぶしゃぶ'
+          firstItem: '辛味噌ラーメン',
+          secondItem: '醤油ラーメン'
         }));
       } else if (req.url === '/enquetes/rice-bread') {
         res.write(pug.renderFile('./form.pug', {
@@ -28,6 +27,12 @@ const server = http.createServer((req, res) => {
           firstItem: '寿司',
           secondItem: 'ピザ'
         }));
+      }　else if (req.url === '/enquetes/yaki-shabu') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: '焼き肉',
+          secondItem: 'しゃぶしゃぶ'
+        }));
       }
       res.end();
       break;
@@ -37,7 +42,7 @@ const server = http.createServer((req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        console.info('投稿: ' + decoded);
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
@@ -47,11 +52,11 @@ const server = http.createServer((req, res) => {
       break;
   }
 }).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
+  console.error('Server Error', e);
 }).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
+  console.error('Client Error', e);
 });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info('Listening on ' + port);
 });
