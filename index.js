8e6c75360005ebf0e689d5e952c28c6ffebd59d9
Hitoshi Tada <hitoshitada@gaia.eonet.ne.jp>
2020-01-05 10:11:15 +0900

    Herokuへのdeploy 時刻表示なし

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const server = http.createServer((req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        console.info(decoded);
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
