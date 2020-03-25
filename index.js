1c914b87fee8c411be261c577d3e819d3eb03ff9
Hondashi2000 <tkdliver0224@docomo.ne.jp>
2020-03-25 19:07:36 +0000

    日付ログの修正

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const http = require('http');
 const pug = require('pug');
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
