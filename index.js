b7d2aff20b0b4cb77660b7962e014135724bcac2
takattty <r9r_celtics_t9t@icloud.com>
2020-04-12 16:56:02 +0900

    fixed require jade to pug

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const server = http.createServer((req, res) => {
   switch (req.method) {
     case 'GET':
       if (req.url === '/enquetes/yaki-shabu') {
-        res.write(pug.renderFile('./form.jade', {
+        res.write(pug.renderFile('./form.pug', {
           path: req.url,
           firstItem: '焼き肉',
           secondItem: 'しゃぶしゃぶ'
         }));
       } else if (req.url === '/enquetes/rice-bread') {
-        res.write(pug.renderFile('./form.jade', {
+        res.write(pug.renderFile('./form.pug', {
           path: req.url,
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
       } else if (req.url === '/enquetes/sushi-pizza') {
-        res.write(pug.renderFile('./form.jade', {
+        res.write(pug.renderFile('./form.pug', {
           path: req.url,
           firstItem: '寿司',
           secondItem: 'ピザ'
