8625883d7855394faa50c4e4c990be298d0c71a8
anazsama <anazsama@outlook.jp>
2020-04-30 07:09:41 +0900

    ログから日付を除去

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const server = http.createServer((req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        console.info('今回の投稿: ' + decoded);
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
