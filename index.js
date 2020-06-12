9384f815bbc99bece4919b4c14f153dd600a2e30
Naoki Nawata <nn.contact.n@gmail.com>
2020-06-12 15:02:24 +0900

    ログの時間を削除

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const http = require('http');
 const pug = require('pug');
 const server = http
   .createServer((req, res) => {
-    const now = new Date();
-    console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+    console.info(' Requested by ' + req.connection.remoteAddress);
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
     });
@@ -45,7 +44,7 @@ const server = http
           .on('end', () => {
             const qs = require('querystring');
             const decoded = decodeURIComponent(rawData);
-            console.info('[' + now + '] 投稿: ' + decoded);
+            console.info(' 投稿: ' + decoded);
             const answer = qs.parse(decoded);
             res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
               answer['name'] + 'さんは' + answer['favorite'] +
@@ -58,12 +57,12 @@ const server = http
     }
   })
   .on('error', e => {
-    console.error('[' + new Date() + '] Server Error', e);
+    console.error(' Server Error', e);
   })
   .on('clientError', e => {
-    console.error('[' + new Date() + '] Client Error', e);
+    console.error(' Client Error', e);
   });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info(' Listening on ' + port);
 });
