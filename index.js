88c0bc86eacf8d1aa1a823fe4fb3420d3cb4716a
Ryuta Watanabe <watanabe@initinc.co.jp>
2021-03-02 18:01:53 +0900

    ログの日付出力を削除

--- index.js
+++ index.js
@@ -1,55 +1,81 @@
-'use strict';
-const http = require('http');
-const pug = require('pug');
+"use strict";
+const http = require("http");
+const pug = require("pug");
 const server = http
   .createServer((req, res) => {
-    const now = new Date();
-    console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+    console.info("Requested by " + req.connection.remoteAddress);
     res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
+      "Content-Type": "text/html; charset=utf-8"
     });
 
     switch (req.method) {
-      case 'GET':
-        if (req.url === '/enquetes/yaki-shabu') {
+      case "GET":
+        if (req.url === "/") {
+          res.write(`
+            <!DOCTYPE html>
+            <html lang="ja">
+              <body>
+                <h1>アンケートフォーム</h1>
+                <a href="/enquetes">アンケート一覧</a>
+              </body>
+            </html>
+          `);
+        } else if (req.url === "/enquetes") {
+          res.write(`
+            <!DOCTYPE html>
+            <html lang="ja">
+              <body>
+                <h1>アンケート一覧</h1>
+                <li><a href="/enquetes/yaki-shabu">焼き肉・しゃぶしゃぶ</a></li>
+                <li><a href="/enquetes/rice-bread">ごはん・パン</a></li>
+                <li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>
+              </body>
+            </html>
+          `);
+        } else if (req.url === "/enquetes/yaki-shabu") {
           res.write(
-            pug.renderFile('./form.pug', {
+            pug.renderFile("./form.pug", {
               path: req.url,
-              firstItem: '焼き肉',
-              secondItem: 'しゃぶしゃぶ'
+              firstItem: "焼き肉",
+              secondItem: "しゃぶしゃぶ"
             })
           );
-        } else if (req.url === '/enquetes/rice-bread') {
+        } else if (req.url === "/enquetes/rice-bread") {
           res.write(
-            pug.renderFile('./form.pug', {
+            pug.renderFile("./form.pug", {
               path: req.url,
-              firstItem: 'ごはん',
-              secondItem: 'パン'
+              firstItem: "ごはん",
+              secondItem: "パン"
             })
           );
-        } else if (req.url === '/enquetes/sushi-pizza') {
-          res.write(pug.renderFile('./form.pug', {
-            path: req.url,
-            firstItem: '寿司',
-            secondItem: 'ピザ'
-          }));
+        } else if (req.url === "/enquetes/sushi-pizza") {
+          res.write(
+            pug.renderFile("./form.pug", {
+              path: req.url,
+              firstItem: "寿司",
+              secondItem: "ピザ"
+            })
+          );
         }
         res.end();
         break;
-      case 'POST':
-        let rawData = '';
+      case "POST":
+        let rawData = "";
         req
-          .on('data', chunk => {
+          .on("data", (chunk) => {
             rawData = rawData + chunk;
           })
-          .on('end', () => {
-            const qs = require('querystring');
+          .on("end", () => {
+            const qs = require("querystring");
             const answer = qs.parse(rawData);
-            const body = answer['name'] + 'さんは' +
-              answer['favorite'] + 'に投票しました';
-            console.info('[' + now + '] ' + body);
-            res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-              body + '</h1></body></html>');
+            const body =
+              answer["name"] + "さんは" + answer["favorite"] + "に投票しました";
+            console.info(body);
+            res.write(
+              '<!DOCTYPE html><html lang="ja"><body><h1>' +
+                body +
+                "</h1></body></html>"
+            );
             res.end();
           });
         break;
@@ -57,13 +83,13 @@ const server = http
         break;
     }
   })
-  .on('error', e => {
-    console.error('[' + new Date() + '] Server Error', e);
+  .on("error", (e) => {
+    console.error("Server Error", e);
   })
-  .on('clientError', e => {
-    console.error('[' + new Date() + '] Client Error', e);
+  .on("clientError", (e) => {
+    console.error("Client Error", e);
   });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info("Listening on " + port);
 });
